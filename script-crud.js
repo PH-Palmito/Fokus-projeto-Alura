2eee3c469d0c01bfd64b371bd56615a93193e839
PH-Palmito <phpalmito@gmail.com>
2025-05-27 23:55:23 -0300

    ,

--- script-crud.js
+++ script-crud.js
@@ -49,7 +49,7 @@ function criarElementoTarefa(tarefa) {
     }
 
     const imagemBotao = document.createElement('img')
-    imagemBotao.setAttribute('src', '/public/imagens/edit.png')
+    imagemBotao.setAttribute('src', '/imagens/edit.png')
     botao.append(imagemBotao)
 
     li.append(svg)
